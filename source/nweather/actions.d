9d79a0c6916992b74e34f7775b529a62e774a8ce
Will Hinson <wmhcell@gmail.com>
2023-04-11 10:16:05 -0400

    Modified search to use overlap coefficient instead of edit distance.

--- source/nweather/actions.d
+++ source/nweather/actions.d
@@ -1,8 +1,11 @@
 module source.nweather.actions;
 
 import std.algorithm.comparison : levenshteinDistance;
+import std.algorithm.mutation : SwapStrategy;
+import std.algorithm.searching : count;
+import std.algorithm.setops : setIntersection, SetIntersection;
 import std.algorithm.sorting : sort;
-import std.string : toUpper;
+import std.string : replace, toUpper;
 
 import dxml.dom;
 import requests;
@@ -14,9 +17,9 @@ import source.nweather.models.target;
 import std.stdio;
 import source.nweather.caching.actions;
 
-struct EditDistance
+struct Similarity
 {
-    float distance;
+    float overlapCoeff;
     Target target;
 }
 
@@ -26,36 +29,60 @@ void fetchWeatherForLocation(string location)
     auto db = getCacheDatabase();
     auto results = db.getKnownTargets();
 
-    // get edit distances for all of the known names
-    EditDistance[] editDistances;
+    // get similarity coefficients for all of the known names
+    Similarity[] similarities;
+    char[] locationArr = location.replace(" ", "").dup;
+    sort(cast(ubyte[]) locationArr);
     foreach (target; results)
     {
-        long longestLength = target.targetName.length > location.length ? target.targetName.length
-            : location.length;
-        editDistances ~= EditDistance(
-            levenshteinDistance(target.targetName.toUpper(), location.toUpper()) / (
-                cast(float) longestLength),
+        char[] targetArr = target.targetName.replace(" ", "").dup;
+        sort(cast(ubyte[]) targetArr);
+
+        long shortestLength = (
+            locationArr.length < targetArr.length ?
+                locationArr.length : targetArr.length
+        );
+
+        auto intersection = setIntersection(locationArr, targetArr);
+        int intersectionLength = 0;
+        while (!intersection.empty)
+        {
+            intersection.popFront();
+            intersectionLength++;
+        }
+        similarities ~= Similarity(
+            (cast(float) intersectionLength) / (cast(float) shortestLength),
             target
         );
     }
-    editDistances.sort!("a.distance < b.distance");
+    similarities.sort!("a.overlapCoeff > b.overlapCoeff");
 
     // get the url of the target xml resource
     string targetUrl;
-    if (editDistances[0].distance <= 0.1)
-    targetUrl = editDistances[0].target.targetUrl;
+    if (location.length > 4 && similarities[0].overlapCoeff > 0.85)
+    targetUrl = similarities[0].target.targetUrl;
     else
         targetUrl = "https://w1.weather.gov/xml/current_obs/" ~ location ~ ".xml";
 
     // get the response data from the remote target
     Request request = Request();
     Response response = request.get(targetUrl);
-    assert(response.code == 200);
+    if (response.code != 200)
+    {
+        writeln("nweather: Unable to fetch weather data for location '", location, "'");
+        writeln("nweather: Target URL was '" ~ targetUrl ~ "', response code was ", response.code);
+        return;
+    }
 
     // parse the response body for a list of localities
     string[string] data;
     auto document = parseDOM(cast(string) response.responseBody);
-    assert(document.children[1].name == "current_observation");
+    if (document.children[1].name != "current_observation")
+    {
+        writeln("nweather: Unable to fetch weather data for location '", location, "'");
+        writeln("nweather: Target URL was '" ~ targetUrl ~ "', <current_observation> tag was missing");
+        return;
+    }
     foreach (element; document.children[1].children)
     {
         // exclude any elements with multiple child elements
@@ -73,11 +100,14 @@ void fetchWeatherForLocation(string location)
     writeln("Temperature:     ", data["temperature_string"]);
     if ("weather" in data)
         writeln("Conditions:      ", data["weather"]);
-    if ("wind_gust_mph" in data)
-        writeln("Wind:            ", data["wind_mph"], " MPH ", data["wind_dir"], " ", data["wind_degrees"],
-    "° (Gusts to ", data["wind_gust_mph"], " MPH)");
+    if ("wind_mph" in data)
+    {
+        if ("wind_gust_mph" in data)
+            writeln("Wind:            ", data["wind_mph"], " MPH ", data["wind_dir"], " ", data["wind_degrees"],
+        "° (Gusts to ", data["wind_gust_mph"], " MPH)");
     else
-        writeln("Wind:            ", data["wind_mph"], " MPH ", data["wind_dir"], " ", data["wind_degrees"], "°");
+            writeln("Wind:            ", data["wind_mph"], " MPH ", data["wind_dir"], " ", data["wind_degrees"], "°");
+    }
     writeln("Pressure:        ", data["pressure_in"], " inHg");
     writeln("Rel. humidity:   ", data["relative_humidity"], "%");
     writeln("Dewpoint:        ", data["dewpoint_f"], "F (", data["dewpoint_c"], "C)");
